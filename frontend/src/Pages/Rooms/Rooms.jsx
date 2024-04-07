6d7d5f39761ad6db73cce637bb50b8e5825d4729
Shekhar <shekharyadav638@gmail.com>
2024-04-07 15:25:30 +0530

    Room Card Created

--- frontend/src/Pages/Rooms/Rooms.jsx
+++ frontend/src/Pages/Rooms/Rooms.jsx
@@ -1,5 +1,43 @@
 import React from "react";
 import styles from "./Rooms.module.css";
+import RoomCard from "../../Components/RoomCard/RoomCard";
+
+const rooms = [
+  {
+    id: 1,
+    topic: "Which framework best for frontend ?",
+    speakers: [
+      {
+        id: 1,
+        name: "John Doe",
+        avatar: "/images/monkey-avatar.png",
+      },
+      {
+        id: 2,
+        name: "Jane Doe",
+        avatar: "/images/monkey-avatar.png",
+      },
+    ],
+    totalPeople: 40,
+  },
+  {
+    id: 4,
+    topic: "Why people use stack overflow?",
+    speakers: [
+      {
+        id: 1,
+        name: "John Doe",
+        avatar: "/images/monkey-avatar.png",
+      },
+      {
+        id: 2,
+        name: "Jane Doe",
+        avatar: "/images/monkey-avatar.png",
+      },
+    ],
+    totalPeople: 40,
+  },
+];
 
 const Rooms = () => {
   return (
@@ -13,7 +51,17 @@ const Rooms = () => {
               <input type="text" className={styles.searchBar}></input>
             </div>
           </div>
-          <div className={styles.right}></div>
+          <div className={styles.right}>
+            <button className={styles.createRoom}>
+              <img src="/images/add-room-icon.png"></img>
+              <span>Create a room</span>
+            </button>
+          </div>
+        </div>
+        <div className={styles.roomsList}>
+          {rooms.map((room) => (
+            <RoomCard key={room.id} room={room} />
+          ))}
         </div>
       </div>
     </>
